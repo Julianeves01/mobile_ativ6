fe43433aaff9d046de1b45d21a4fcee8977ea4b0
Julianeves01 <julianvs2007@gmail.com>
2025-04-03 21:39:05 -0300

    update TabNavigator.js

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,34 +1,30 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import TabScreen from '../pages/TabScreen';
-import Tab2 from '../pages/Tab2';
-import Icon from 'react-native-vector-icons/Ionicons';
+import { View, Text } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
+function Screen1() {
+    return (
+        <View>
+            <Text>Screen 1</Text>
+        </View>
+    );
+}
+
+function Screen2() {
+    return (
+        <View>
+            <Text>Screen 2</Text>
+        </View>
+    );
+}
+
 export default function TabNavigator() {
     return (
-        <Tab.Navigator initialRouteName="TabScreen">
-            <Tab.Screen 
-                name="TabScreen"
-                component={TabScreen}
-                options={{
-                    tabBarIcon: ({ color, size }) => (
-                        <Icon name="home" color={color} size={size} />
-                    ),
-                    tabBarLabel: 'Início', // Adiciona um rótulo personalizado
-                }}
-            />
-            <Tab.Screen 
-                name="Tab2" 
-                component={Tab2} 
-                options={{
-                    tabBarIcon: ({ color, size }) => (
-                        <Icon name="settings" color={color} size={size} />
-                    ),
-                    tabBarLabel: 'Configurações', // Adiciona um rótulo personalizado
-                }}
-            />
-        </Tab.Navigator>
+        <Tab.navigator>
+            <Tab.Screen name="Screen1" component={Screen1} />
+            <Tab.Screen name="Screen2" component={Screen2} />
+        </Tab.navigator>
     );
 }
